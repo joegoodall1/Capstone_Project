580465dc7cd71dc3d5c741fc00e75c200cd57bef
joegoodall1 <joegoodall1@gmail.com>
2016-02-21 12:10:50 +0000

    Restrict the amount of weight (<1000) and reps (<100) that can be entered

--- app/src/main/java/com/getstrength/myapplication/EditorActivity.java
+++ app/src/main/java/com/getstrength/myapplication/EditorActivity.java
@@ -129,27 +129,15 @@ public class EditorActivity extends ActionBarActivity {
     }
 
     public void addSet(View view) {
-
-        //inflateEditRow(LAYOUT_INFLATER_SERVICE);
         LayoutInflater inflater = (LayoutInflater) getSystemService(this.LAYOUT_INFLATER_SERVICE);
         final View rowView = inflater.inflate(R.layout.new_set, null);
-        /*final TextView set = (TextView) rowView.findViewById(R.id.textView1);
-        final EditText weight = (EditText) rowView.findViewById(R.id.editText1);
-        final EditText reps = (EditText) rowView.findViewById(R.id.editText2);*/
         mContainerView.addView(rowView, mContainerView.getChildCount());
-
-        Toast.makeText(EditorActivity.this, "Add set", Toast.LENGTH_SHORT).show();
     }
 
 
 
     public void removeSet(View view) {
-        Toast.makeText(EditorActivity.this, "Remove set", Toast.LENGTH_SHORT).show();
         LinearLayout ll = (LinearLayout) findViewById(R.id.new_set_layout);
-        //final View rowView = inflater.inflate(R.layout.new_set, null);
-        /*final TextView set = (TextView) rowView.findViewById(R.id.textView1);
-        final EditText weight = (EditText) rowView.findViewById(R.id.editText1);
-        final EditText reps = (EditText) rowView.findViewById(R.id.editText2);*/
         mContainerView.removeView(ll);
     }
 
