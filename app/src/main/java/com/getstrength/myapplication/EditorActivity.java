e587de3f6b63dcd27fff310ecedcd99d6e37d226
joegoodall1 <joegoodall1@gmail.com>
2016-03-02 03:11:36 +0000

    Added date to appbar in editor activity. todo: Make appbar clickable and extract data from edittext

--- app/src/main/java/com/getstrength/myapplication/EditorActivity.java
+++ app/src/main/java/com/getstrength/myapplication/EditorActivity.java
@@ -1,6 +1,5 @@
 package com.getstrength.myapplication;
 
-import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
@@ -10,17 +9,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.getstrength.myapplication.model.Exercise;
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.UUID;
 
 import io.realm.Realm;
@@ -38,6 +39,7 @@ public class EditorActivity extends AppCompatActivity {
     private TextView setNumber;
     private int num = 1;
     private Realm realm;
+    private Date date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,13 +51,12 @@ public class EditorActivity extends AppCompatActivity {
                 .name("exerciseData.realm")
                 .build();
 
-
         //Make this Realm the default
         Realm.setDefaultConfiguration(config);
 
         realm = Realm.getDefaultInstance();
 
-        //create some tasks
+        /*//create some tasks
         realm.beginTransaction();
         Exercise t = realm.createObject(Exercise.class);
         t.setId(UUID.randomUUID().toString());
@@ -63,24 +64,28 @@ public class EditorActivity extends AppCompatActivity {
         t.setWeight(100);
         t.setSets(3);
         t.setExerciseName("Bench press");
-        realm.commitTransaction();
+        realm.commitTransaction();*/
 
-        RealmResults<Exercise> exercises = realm.allObjects(Exercise.class);
+        /*RealmResults<Exercise> exercises = realm.allObjects(Exercise.class);
         for (Exercise exercise : exercises) {
             Log.d("Hello", String.format("ID: %s, Reps: %s, Weight: %s, Sets: %s, Exercise: %s", exercise.getId(), exercise.getReps(), exercise.getWeight(), exercise.getSets(), exercise.getExerciseName()));
-        }
+        }*/
 
-        editor = (EditText) findViewById(R.id.editText);
+        editor = (EditText) findViewById(R.id.datePicker);
         mContainerView = (LinearLayout) findViewById(R.id.parentView);
         setNumber = (TextView) findViewById(R.id.textView2);
 
         Intent intent = getIntent();
 
+        Date curDate = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        String DateToStr = format.format(curDate);
+
         Uri uri = intent.getParcelableExtra(NotesProvider.CONTENT_ITEM_TYPE);
 
         if (uri == null) {
             action = Intent.ACTION_INSERT;
-            setTitle(getString(R.string.new_note));
+            setTitle(DateToStr);
         } else {
             action = Intent.ACTION_EDIT;
             noteFilter = DBOpenHelper.NOTE_ID + "=" + uri.getLastPathSegment();
@@ -92,6 +97,16 @@ public class EditorActivity extends AppCompatActivity {
             editor.setText(oldText);
             editor.requestFocus();
         }
+
+        /*final int abTitleId = getResources().getIdentifier(DateToStr, "id", getPackageName());
+        findViewById(abTitleId).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //Do something
+            }
+        });*/
+
 
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
         // Create an ArrayAdapter using the string array and a default spinner layout
@@ -111,62 +126,26 @@ public class EditorActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
 
-        switch (id) {
-            case android.R.id.home:
-                finishEditing();
-                break;
-            case R.id.action_delete:
-                deleteNote();
-                break;
-        }
 
-        return true;
-    }
-
-    private void deleteNote() {
+    /*private void deleteNote() {
         getContentResolver().delete(NotesProvider.CONTENT_URI,
                 noteFilter, null);
         Toast.makeText(this, getString(R.string.note_deleted),
                 Toast.LENGTH_SHORT).show();
         setResult(RESULT_OK);
         finish();
-    }
+    }*/
 
-    private void finishEditing() {
-        String newText = editor.getText().toString().trim();
 
-        switch (action) {
-            case Intent.ACTION_INSERT:
-                if (newText.length() == 0) {
-                    setResult(RESULT_CANCELED);
-                } else {
-                    insertNote(newText);
-                }
-                break;
-            case Intent.ACTION_EDIT:
-                if (newText.length() == 0) {
-                    deleteNote();
-                } else if (oldText.equals(newText)) {
-                    setResult(RESULT_CANCELED);
-                } else {
-                    updateNote(newText);
-                }
 
-        }
-        finish();
-    }
-
-    private void updateNote(String noteText) {
+    /*private void updateNote(String noteText) {
         ContentValues values = new ContentValues();
         values.put(DBOpenHelper.NOTE_TEXT, noteText);
         getContentResolver().update(NotesProvider.CONTENT_URI, values, noteFilter, null);
         Toast.makeText(this, getString(R.string.note_updated), Toast.LENGTH_SHORT).show();
         setResult(RESULT_OK);
-    }
+    }*/
 
     public void addSet(View view) {
         num++;
@@ -184,17 +163,45 @@ public class EditorActivity extends AppCompatActivity {
         }
     }
 
-    private void insertNote(String noteText) {
-        ContentValues values = new ContentValues();
-        values.put(DBOpenHelper.NOTE_TEXT, noteText);
-        getContentResolver().insert(NotesProvider.CONTENT_URI, values);
-        setResult(RESULT_OK);
+    public void onClick1(View view) {
+        Calendar now = Calendar.getInstance();
+        final DatePickerDialog d = DatePickerDialog.newInstance(
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePickerDialog datePickerDialog, int year, int monthOfYear, int dayOfMonth) {
+                        Calendar checkedCalendar = Calendar.getInstance();
+                        checkedCalendar.set(year, monthOfYear, dayOfMonth);
+                        date = checkedCalendar.getTime();
+                        action = Intent.ACTION_INSERT;
+                        setTitle(DateFormatter.convertDateToString(date));
+                    }
+                },
+                now.get(Calendar.YEAR),
+                now.get(Calendar.MONTH),
+                now.get(Calendar.DAY_OF_MONTH)
+        );
+        d.setMaxDate(now);
+        d.show(getFragmentManager(), action);
     }
 
     @Override
     public void onBackPressed() {
-        finishEditing();
+        //create some tasks
+        realm.beginTransaction();
+        Exercise t = realm.createObject(Exercise.class);
+        t.setId(UUID.randomUUID().toString());
+        t.setReps(10);
+        t.setWeight(75);
+        t.setSets(2);
+        t.setExerciseName("deadlift");
+        realm.commitTransaction();
+
+        RealmResults<Exercise> exercises = realm.allObjects(Exercise.class);
+        for (Exercise exercise : exercises) {
+            Log.d("Hello", String.format("ID: %s, Reps: %s, Weight: %s, Sets: %s, Exercise: %s", exercise.getId(), exercise.getReps(), exercise.getWeight(), exercise.getSets(), exercise.getExerciseName()));
+        }
     }
+
 
 }
 
