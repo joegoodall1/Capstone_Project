2e18e5f4c35b13cbb98633d11bc3c1a5e690c725
joegoodall1 <joegoodall1@gmail.com>
2016-03-10 19:43:21 +0000

    Listview added to History

---
+++ app/src/main/java/com/getstrength/myapplication/HistoryFragment.java
@@ -0,0 +1,69 @@
+package com.getstrength.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.List;
+
+/**
+ * Created by Joe on 10/03/2016.
+ */
+public class HistoryFragment extends ListFragment implements AdapterView.OnItemClickListener {
+
+    private List<String> mExercises;
+
+    public HistoryFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_history, container, false);
+
+
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ExerciseStore.initialise(getContext());
+        mExercises = ExerciseStore.getInstance().getExerciseDates();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, mExercises);
+        ListView listView = (ListView) super.getView().findViewById(android.R.id.list);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(super.getContext(), "Item clicked: " + mExercises.get(position), Toast.LENGTH_SHORT).show();
+    }
+}
